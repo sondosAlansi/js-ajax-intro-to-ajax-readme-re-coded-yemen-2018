5dce7f7992b9e2b2c6d1549ab047af1607a41046
sondos alansi <sondos.alansi@yahoo.com>
2018-10-16 21:00:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function showRepositories() {
   
   console.log(this.responseText);
+    let repoList = '<ul>';
+  for (var i = 0; i < this.responseText.length; i++) {
+    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
+  }
+  repoList += '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
 }
 
 function getRepositories() {
